f0647edb4c0df227a8f2aea03a9a6a86abf028c3
Marissa <marissa.shieh@gmail.com>
2019-03-28 16:16:29 -0700

    Serve up static public folder confirming front-end is connected

--- server/index.js
+++ server/index.js
@@ -3,4 +3,6 @@ const express = require('express');
 const app = express();
 const port = 3002;
 
+app.use(express.static('public'));
+
 app.listen(port, () => { console.log(`Listening on port ${port}`); });
