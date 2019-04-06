0b090ea4dabbbc57e092bb7014cc2b4e0af37fc8
Marissa <marissa.shieh@gmail.com>
2019-04-05 17:12:32 -0700

    Implement multiple CSS modules on the same component

--- client/components/Review.jsx
+++ client/components/Review.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import moment from 'moment';
 import styles from './Review.module.css';
+import sharedStyles from './Component.module.css';
 
 function Review(props) {
   console.log(props.review.review_text);
   return (
     <div class='review'>
       <img src={props.review.user_photo}></img>
-    <b>{props.review.user_name}</b>
-    <div>{moment(props.review.review_date).format('MMMM YYYY')}</div>
+    <b class='name'>{props.review.user_name}</b>
+    <div class='date'>{moment(props.review.review_date).format('MMMM YYYY')}</div>
     <p>
       {props.review.review_text}
     </p>
