ca35204f0e99c3f7fcc6368def4b7a5ad43be0eb
Marissa <marissa.shieh@gmail.com>
2019-04-08 15:44:12 -0700

    Fix spacing w/ total reviews and stars

--- client/components/TotalReviews.jsx
+++ client/components/TotalReviews.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import styles from './TotalReviews.module.css';
 import sharedStyles from './Component.module.css';
-import stars from './Stars.module.css'
+import stars from './Stars.module.css';
 
 function TotalReviews(props) {
   return (
     <div className='totalReviews'>
-      <div className='reviewTitle'>{props.reviews.length} Reviews </div>
-      <span className={`stars stars-${(Math.ceil(props.stars+1)/10)*100}`}> ★★★★★ </span>
+      <h3 className='reviewTitle'>{props.reviews.length} Reviews </h3>
+      <div className={`stars stars-${(Math.ceil(props.stars+1)/10)*100}`}> ★★★★★ </div>
     </div>
   );
 }
