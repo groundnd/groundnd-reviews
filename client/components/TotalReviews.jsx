1045e79a82c079a530484e919988c8f230be049f
Marissa <marissa.shieh@gmail.com>
2019-04-07 17:17:39 -0700

    Implement stars and their css

--- client/components/TotalReviews.jsx
+++ client/components/TotalReviews.jsx
@@ -1,18 +1,13 @@
 import React from 'react';
 import styles from './TotalReviews.module.css';
 import sharedStyles from './Component.module.css';
+import stars from './Stars.module.css'
 
 function TotalReviews(props) {
   return (
     <div className='totalReviews'>
-      <div className='reviewTitle'>{props.reviews.length} Reviews</div>
-      <div className='totalStars'>
-        <div className='star uncoloredStar'>
-        </div>
-        <div className='star coloredStar'>
-          {props.stars}
-        </div>
-      </div>
+      <div className='reviewTitle'>{props.reviews.length} Reviews </div>
+      <span className={`stars stars-${Math.ceil(Math.round(props.stars)*10)}`}> ★★★★★ </span>
     </div>
   );
 }
