901c4930447e1d92b96d204bc773274124e1c137
Marissa <marissa.shieh@gmail.com>
2019-04-08 19:34:53 -0700

    Align pagination buttons horizontally

--- client/components/Pagination.jsx
+++ client/components/Pagination.jsx
@@ -1,18 +1,61 @@
 import React from 'react';
 import PaginationBtn from './PaginationBtn';
+import styles from './PageNavBtns.module.css';
+import sharedStyles from './Component.module.css';
+
 
 function Pagination (props) {
   const {maxPage, newPageFn, pageNum} = props;
   const pageNumbers = [];
-  for (let page = 1; page <= maxPage; page++) {
-    pageNumbers.push(page);
+
+  const appendMaxPage = () => {
+    pageNumbers.push('...');
+    pageNumbers.push(maxPage);
+  };
+
+  const appendMinPage = () => {
+    pageNumbers.unshift('...');
+    pageNumbers.unshift(1);
+  };
+
+  if (maxPage < 4) {
+    for (let page = 1; page <= maxPage; page++) {
+      pageNumbers.push(page);
+    }
+  } else if (pageNum === 1 || pageNum === 2) {
+    for (let page = 1; page <= 3; page++) {
+      pageNumbers.push(page);
+    }
+    appendMaxPage();
+  } else if (pageNum === 3 || pageNum === 4) {
+    for (let page = 1; page <= pageNum+1; page++) {
+      pageNumbers.push(page);
+    }
+    appendMaxPage();
+  } else if (maxPage - pageNum === 1 || pageNum === maxPage) {
+    for (let page = maxPage; page > maxPage-3; page--) {
+      pageNumbers.unshift(page);
+    }
+    appendMinPage();
+  } else if (maxPage - pageNum === 2 || maxPage - pageNum === 3) {
+    for (let page = maxPage; page > maxPage - pageNum + 1; page--) {
+      pageNumbers.unshift(page);
+    }
+    appendMinPage();
+  } else {
+    pageNumbers.push(pageNum - 1);
+    pageNumbers.push(pageNum);
+    pageNumbers.push(pageNum + 1);
+    appendMinPage();
+    appendMaxPage();
   }
+  console.log(pageNumbers);
   return (
-    <div>
+    <nav className="pagination">
       {pageNumbers.map((page) => {
         return <PaginationBtn pageNum={page} newPageFn={newPageFn} key={page} />
       })}
-    </div>
+    </nav>
   );
 }
 
--- client/components/PaginationBtn.jsx
+++ client/components/PaginationBtn.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import styles from './PageNavBtns.module.css';
+import sharedStyles from './Component.module.css';
 
 function PaginationBtn (props) {
   const {pageNum, newPageFn} = props;
@@ -6,9 +8,18 @@ function PaginationBtn (props) {
     newPageFn(id);
   }
 
+  let pageButton;
+  if (pageNum === '...') {
+    pageButton = <button type="button" className='paginationBtn' >{pageNum}</button>;
+  } else {
+    pageButton = <button type="button" className='paginationBtn' id={pageNum} onClick={(e)=>{clickHandler(e.target.id)}}>{pageNum}</button>;
+  }
+
   return (
-    <button className='paginationBtn' id={pageNum} onClick={(e)=>{clickHandler(e.target.id)}}>{pageNum}</button>
-  )
+    <div>
+      {pageButton}
+    </div>
+  );
 }
 
 export default PaginationBtn;
