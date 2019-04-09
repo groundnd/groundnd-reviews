db4dc6ff6b29f8fa1d498176a373289808579e42
Marissa <marissa.shieh@gmail.com>
2019-04-09 16:55:10 -0700

    Fix styling for ratings container

--- client/components/Rating.jsx
+++ client/components/Rating.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import styles from './Ratings.module.css';
-import stars from './Stars.module.css';
 
 function Rating(props) {
   const { value, type } = props;
+  const starPercentage = (Math.ceil(value + 1) / 10) * 100;
   return (
-    <div className="criteriaAndStars">
+    <div className={styles.criteriaAndStars}>
       <div>{type}</div>
-      <span className={`stars stars-${(Math.ceil(value + 1) / 10) * 100}`}> ★★★★★ </span>
+      <span className={[styles.stars, styles[`stars-${starPercentage}`]].join(' ')}> ★★★★★ </span>
     </div>
   );
 }
