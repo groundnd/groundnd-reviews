c8bbb8243e15c90391336ed7223fdeec1494c58b
Marissa <marissa.shieh@gmail.com>
2019-04-08 22:09:08 -0700

    Implement props destructuring

--- client/components/Ratings.jsx
+++ client/components/Ratings.jsx
@@ -4,13 +4,15 @@ import styles from './Ratings.module.css';
 import sharedStyles from './Component.module.css';
 
 function Ratings(props) {
+  const { foundAverage, reviews, calculateAvg, ratings } = props;
+
   // if the averages have NOT been calculated before && we have data from DB, then:
-  if(!props.foundAverage && props.reviews.length > 0) {  
-    const numReviews = props.reviews.length;
+  if(!foundAverage && reviews.length > 0) {  
+    const numReviews = reviews.length;
     let accuracy, communication, cleanliness, location, checkIn, value;
     accuracy = communication = cleanliness = location = checkIn = value = 0;
 
-    props.reviews.forEach((review) => {
+    reviews.forEach((review) => {
       // NOTE: The ratings are stored as Buffers instead of Numbers in MongoDB
       // to take up less memory, and as an added challenge. However, it means
       // I have to pull out my desired value via the syntax seen below.
@@ -32,20 +34,20 @@ function Ratings(props) {
       checkIn: checkIn/numReviews,
       value: value/numReviews,
     };
-    props.calculateAvg(avgRating, calcRatings);
+    calculateAvg(avgRating, calcRatings);
   }
 
   return (
     <div className='ratingsContainer'>
       <span className='ratingTriple ratingsLeft'>
-        <Rating key="accuracyRating" type="Accuracy" value={props.ratings.accuracy} />
-        <Rating key="communicationRating" type="Communication" value={props.ratings.communication} />
-        <Rating key="cleanlinessRating" type="Cleanliness" value={props.ratings.cleanliness} />
+        <Rating key="accuracyRating" type="Accuracy" value={ratings.accuracy} />
+        <Rating key="communicationRating" type="Communication" value={ratings.communication} />
+        <Rating key="cleanlinessRating" type="Cleanliness" value={ratings.cleanliness} />
       </span>
       <span className='ratingTriple ratingsRight'>
-        <Rating key="locationRating" type="Location" value={props.ratings.location} />
-        <Rating key="checkInRating" type="Check-in" value={props.ratings.checkIn} />
-        <Rating key="valueRating" type="Value" value={props.ratings.value} />
+        <Rating key="locationRating" type="Location" value={ratings.location} />
+        <Rating key="checkInRating" type="Check-in" value={ratings.checkIn} />
+        <Rating key="valueRating" type="Value" value={ratings.value} />
       </span>
     </div>
   );
