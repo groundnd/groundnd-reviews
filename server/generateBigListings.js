184dbda0ab970c275bdcd80ff8c52d83a8a3d5bd
Pha-Le Nguyen <phalenguyen15@gmail.com>
2019-04-19 20:30:15 -0700

    update data generating script.

---
+++ server/generateBigListings.js
@@ -0,0 +1,117 @@
+
+const faker = require('faker');
+const fs = require('fs');
+
+// const { Review } = require('./db.js');
+
+const reviewText = () => {
+  let reviewParagraphsArr = [];
+  const reviewLength = 1;
+  for (let paragraphs = 0; paragraphs < reviewLength; paragraphs++) {
+    reviewParagraphsArr.push(faker.lorem.paragraph());
+  }
+  return reviewParagraphsArr.join('\n');
+};
+
+// const generateReviews = () => {
+//   const reviews = {
+//     reviewId: Math.ceil(Math.random() * 10000000),
+//     user_name: faker.name.findName(),
+//     user_photo: faker.image.avatar(),
+//     review_text: reviewText(),
+//     accuracy: Math.ceil(Math.random() * 5),
+//     communication: Math.ceil(Math.random() * 5),
+//     cleanliness: Math.ceil(Math.random() * 5),
+//     location: Math.ceil(Math.random() * 5),
+//     check_in: Math.ceil(Math.random() * 5),
+//     value: Math.ceil(Math.random() * 5),
+//     review_date: faker.date.past(),
+//   };
+//   return reviews;
+// };
+
+// const generateBigListings = () => {
+//   let abodeCount = 0;
+
+//   for (let abode = 1; abode < 10000000; abode++) {
+//     const listing = new Review({
+//       abode_id: abode,
+//       abode_name: faker.lorem.words(),
+//       reviews: generateReviews(),
+//     }); 
+//     console.log(JSON.stringify(listing));
+//   }
+//   process.exit();
+// };
+// generateBigListings();
+
+// if (Review.collection) {
+//   Review.db.dropCollection('reviews', (err) => {
+//     if (err) {
+//       console.log('failed to drop existing collection from MongoDB, ', err);
+//     } else {
+//       console.log('successfully removed collection and now reseeding');
+//       generateBigListings();
+//     }
+//   });
+// } else {
+//   console.log('seeding data for the first time');
+//   generateBigListings();
+// }
+const generateReviews = () => {
+  const numOfReviews = Math.floor(Math.random() * 5);
+  const reviews = [];
+  for (let count = 0; count < numOfReviews; count++) {
+    reviews.push({
+      user_name: faker.name.findName(),
+      user_photo: faker.image.avatar(),
+      review_text: reviewText(),
+      accuracy: Math.ceil(Math.random() * 5),
+      communication: Math.ceil(Math.random() * 5),
+      cleanliness: Math.ceil(Math.random() * 5),
+      location: Math.ceil(Math.random() * 5),
+      check_in: Math.ceil(Math.random() * 5),
+      value: Math.ceil(Math.random() * 5),
+      review_date: faker.date.past(),
+    });
+  }
+  return reviews;
+};
+const listing = () => {
+  const obj = {
+    abode_id: 0,
+    abode_name: faker.lorem.words(),
+    reviews: generateReviews(),
+  };
+  return obj;
+};
+// console.log(listing)
+const writeData = () => {
+  let stream = fs.createWriteStream('./data1.csv');
+  let i = 10000000;
+
+  const write = () => {
+    let ok = true;
+    do {
+      if (i === 0) {
+        const dataSet = listing();
+        dataSet.abode_id = i;
+        stream.write(JSON.stringify(dataSet));
+        stream.end();
+      } else {
+        const dataSet = listing();
+        dataSet.abode_id = i;
+        ok = stream.write(`${JSON.stringify(dataSet)} \n`);
+      }
+      i -= 1;
+    } while (i > 0 && ok);
+    if (i > 0) {
+      stream.once('drain', write);
+    }
+  };
+  write();
+};
+writeData();
+
+
+// module.exports.generateBigListings = generateBigListings;
