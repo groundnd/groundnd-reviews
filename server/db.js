133c4f3e669ae1c9e556b7f209926ae4ae1bf982
Pha-Le Nguyen <phalenguyen15@gmail.com>
2019-04-26 20:03:40 -0700

    update database.

--- server/db.js
+++ server/db.js
@@ -1,29 +1,29 @@
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/abodely', { useNewUrlParser: true, useCreateIndex: true });
-const db = mongoose.connection;
-// localhost
-// 172.17.0.2
+// mongoose.connect('mongodb://localhost/abodely', { useNewUrlParser: true, useCreateIndex: true });
+// const db = mongoose.connection;
+// // localhost
+// // 172.17.0.2
 
-const oneReviewSchema = new mongoose.Schema({
-  user_name: { type: String, unique: true },
-  user_photo: String,
-  review_text: String,
-  accuracy: Buffer,
-  communication: Buffer,
-  cleanliness: Buffer,
-  location: Buffer,
-  check_in: Buffer,
-  value: Buffer,
-  review_date: Date,
-});
+// const oneReviewSchema = new mongoose.Schema({
+//   user_name: { type: String, unique: true },
+//   user_photo: String,
+//   review_text: String,
+//   accuracy: Buffer,
+//   communication: Buffer,
+//   cleanliness: Buffer,
+//   location: Buffer,
+//   check_in: Buffer,
+//   value: Buffer,
+//   review_date: Date,
+// });
 
-const reviewSchema = new mongoose.Schema({
-  abode_id: { type: Number, unique: true },
-  abode_name: String,
-  reviews: [oneReviewSchema],
-});
-const Review = mongoose.model('Review', reviewSchema);
+// const reviewSchema = new mongoose.Schema({
+//   abode_id: { type: Number, unique: true },
+//   abode_name: String,
+//   reviews: [oneReviewSchema],
+// });
+// const Review = mongoose.model('Review', reviewSchema);
 
-module.exports.Review = Review;
-module.exports.db = db;
+// module.exports.Review = Review;
+// module.exports.db = db;
