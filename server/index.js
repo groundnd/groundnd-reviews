938c6a680212347262daca9e1e4aafae4c9508a1
Pha-Le Nguyen <phalenguyen15@gmail.com>
2019-04-26 19:52:50 -0700

    update server.

--- server/index.js
+++ server/index.js
@@ -1,26 +1,29 @@
+const newrelic = require('newrelic');
 const express = require('express');
 const path = require('path');
 const { Review } = require('./db.js');
 const compression = require('compression');
+const db = require('./db.1');
 
 const app = express();
 const port = 3002;
 
 app.use(compression());
-app.use(express.static('public'));
+app.use('/abodes/:abode_id', express.static('public'));
 
-app.get('/abodes/:abode_id/reviews', (req, res) => {
-  Review.findOne({ abode_id: parseInt(req.params.abode_id) }, (err, abodeInfo) => {
+app.get('/api/abodes/:abode_id', (req, res) => {
+  const abodeId = req.params.abode_id;
+  db.query(`SELECT * FROM reviews WHERE abodeid=${abodeId};`, '', (err, abodeInfo) => {
     if (err) {
       console.log('failed to find this listing', err);
     } else {
-      res.send(abodeInfo);
+      res.send(JSON.stringify(abodeInfo.rows));
     }
   });
 });
 
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/index.html'));
-});
+// app.get('/*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../public/index.html'));
+// });
 
 app.listen(port, () => { console.log(`Listening on port ${port}`); });
